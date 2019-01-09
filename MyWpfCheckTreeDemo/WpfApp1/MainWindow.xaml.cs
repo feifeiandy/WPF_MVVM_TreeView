3927c316a8859fb781150a1b007a362d3ef9691b
Q <feifeiandy@outlook.com>
2019-01-09 14:37:59 +0800

    1

---
+++ MyWpfCheckTreeDemo/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        StudentModel studentModel = new StudentModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = studentModel;
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Student : INotifyPropertyChanged
+    {
+        public int ID { get; set; }
+        private bool? isChecked;
+        public bool? IsChecked
+        {
+            get { return isChecked; }
+            set
+            {
+                isChecked = value;
+                this.OnPropertyChanged("IsChecked");
+            }
+
+        }
+        private string name;
+        private bool isSelect;
+        public bool IsSelect
+        {
+            get { return isSelect; }
+            set
+            {
+                isSelect = value;
+                this.OnPropertyChanged("IsSelect");
+                if (this.IsSelect)
+                {
+                    this.SelectStudentItem = this;
+                    //Student student = new Student();
+                    //student.name = "xx";
+                    //this.Stus.Add(student);
+                }
+                else
+                {
+                    this.SelectStudentItem = null;
+                }
+                MessageBox.Show(this.SelectStudentItem.Name);
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                this.OnPropertyChanged("Name");
+            }
+        }
+        private Student selectStudentItem;
+        public Student SelectStudentItem
+        {
+            get { return selectStudentItem; }
+            set
+            {
+                selectStudentItem = value;
+                this.OnPropertyChanged("SelectStudentItem");
+            }
+        }
+        private ObservableCollection<Student> stus;
+        public ObservableCollection<Student> Stus
+        {
+            get { return stus; }
+            set
+            {
+                stus = value;
+                this.OnPropertyChanged("Stus");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public Student()
+        {
+            this.Stus = new ObservableCollection<Student>();
+        }
+    }
+
+    public class StstusConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return ToggleState.Indeterminate;
+            }
+            bool flag = (bool)value;
+            if (flag == true)
+            {
+                return ToggleState.On;
+            }
+            return ToggleState.Off;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            ToggleState flag = (ToggleState)value;
+            if (flag == ToggleState.On)
+            {
+                return true;
+            }
+            if (flag == ToggleState.Off)
+            {
+                return false;
+
+            }
+            return null;
+        }
+    }
+}
---
+++ MyWpfCheckTreeDemo/WpfApp1/StudentModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class StudentModel:INotifyPropertyChanged
+    {
+        public Student newStudent;
+        public RelayCommand btnCommand;
+        private ObservableCollection<Student> students;
+        public ObservableCollection<Student> Students
+        {
+            get { return students; }
+            set { students = value;
+                this.OnPropertyChanged("Students");
+            }
+        }
+        public void run()
+        {
+            newStudent = new Student();
+            newStudent.Name = "xmx";
+        }
+        
+        public StudentModel()
+        {
+            Students = new ObservableCollection<Student>();
+          
+            Student s1= new Student() {ID=1,Name="1"};
+           
+            Student s2= new Student() {ID=2,Name="2"};
+            s1.Stus.Add(s2);
+
+            Student s3 = new Student() { ID = 2, Name = "3",IsChecked=true };
+            s2.Stus.Add(s3);
+
+            Student s4 = new Student() { ID = 2, Name = "4" };
+            s2.Stus.Add(s4);
+
+            Students.Add(s1);
+
+            this.btnCommand = new RelayCommand(run);
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+ 
+
+    }
+
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RelayCommand(Action a)
+        {
+            a();
+        }
+    }
+}
